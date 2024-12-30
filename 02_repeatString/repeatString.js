dd9de0ea7a131b5eb65d1abd4899ae8a6954463a
David Park <DavidPark2027@u.northwestern.edu>
2024-12-29 19:24:23 -0600

    JS tasks

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    else {
+        let repeatString = "";
+        for (let i = 0; i < times; i++) {
+            repeatString += string;
+        }
+    
+        return repeatString;
+    }
 };
 
 // Do not edit below this line
